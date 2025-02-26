8bee5d31af6fc36f51faabe51e77234578146b0e
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-02-26 09:54:24 +0100

    ✨ F01 : Set Up for auth

---
+++ src/lib/auth.config.ts
@@ -0,0 +1,42 @@
+// auth.config.ts
+import Credentials from "next-auth/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+import bcrypt from "bcryptjs";
+import { prisma } from "./prisma";
+
+export default {
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // 1. Vérifiez si l'utilisateur existe dans la base de données
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error("Utilisateur non trouvé.");
+        }
+
+        // 2. Vérifiez si le mot de passe est correct
+        const isValidPassword = await bcrypt.compare(
+          credentials.password as string,
+          user.password
+        );
+
+        if (!isValidPassword) {
+          throw new Error("Mot de passe incorrect.");
+        }
+
+        // 3. Retournez l'utilisateur si tout est valide
+        return user;
+      },
+    }),
+  ],
+  session: { strategy: "jwt" }, // Utilisez JWT pour la session
+  secret: process.env.AUTH_SECRET, // Clé secrète pour signer les tokens
+} satisfies NextAuthConfig;

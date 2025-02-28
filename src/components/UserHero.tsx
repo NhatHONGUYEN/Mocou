e331c646dfe4f341fe6c332444131b10ed8e7277
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-02-28 18:13:55 +0100

    📱 F03: work on resposive for mobile

--- src/components/UserHero.tsx
+++ src/components/UserHero.tsx
@@ -20,15 +20,15 @@ export default function UserHero() {
                 compétences linguistiques.
               </p>
               <div>
-                <div className="flex flex-col items-center gap-4 lg:flex-row">
-                  <div className="shrink-0 w-full sm:w-auto">
-                    <Button asChild className="w-full sm:w-auto">
+                <div className="flex flex-col items-center gap-4 md:flex-row">
+                  <div className="shrink-0 w-full md:w-auto">
+                    <Button asChild className="w-full md:w-auto">
                       <Link href="/game">Play</Link>
                     </Button>
                   </div>
                   <Button
                     asChild
-                    className="group flex h-fit items-center gap-2 w-full sm:w-auto" // Ajoutez w-full pour mobile
+                    className="group flex h-fit items-center gap-2 w-full md:w-auto" // Ajoutez w-full pour mobile
                   >
                     <Link href="/leaderboard">
                       <p>Voir les scores</p>
@@ -39,7 +39,7 @@ export default function UserHero() {
               </div>
             </div>
           </div>
-          <div className="flex justify-end">
+          <div className="flex md:mx-auto lg:justify-end">
             <Card>
               <CardContent className="p-0">
                 <Image

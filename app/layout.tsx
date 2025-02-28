6c898bd419df0b06550d6346e262e9af99cbdacd
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-02-28 18:12:31 +0100

    📱 F03: work on resposive for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       >
         <SessionProvider>
           <QueryProvider>
-            <div className="mx-auto w-72 sm:w-auto max-w-6xl">
+            <div className="mx-auto w-72 sm:w-96 md:w-[600px] lg:w-[800px] xl:w-full max-w-6xl">
               <Header />
               {children}
             </div>
--- src/components/NoUserHero.tsx
+++ src/components/NoUserHero.tsx
@@ -21,15 +21,15 @@ export default function NoUserHero() {
                 linguistiques. Amusez-vous et progressez à votre rythme !
               </p>
               <div>
-                <div className="flex flex-col items-center gap-4 lg:flex-row">
-                  <div className="shrink-0 w-full sm:w-auto">
-                    <Button asChild className="w-full sm:w-auto">
+                <div className="flex flex-col items-center gap-4 md:flex-row">
+                  <div className="shrink-0 w-full md:w-auto">
+                    <Button asChild className="w-full md:w-auto">
                       <Link href="/game">Jouer</Link>
                     </Button>
                   </div>
                   <Button
                     asChild
-                    className="group flex h-fit items-center gap-2 w-full sm:w-auto"
+                    className="group flex h-fit items-center gap-2 w-full md:w-auto"
                   >
                     <Link href="/sign-up">
                       <p>Voir plus</p>
@@ -40,7 +40,7 @@ export default function NoUserHero() {
               </div>
             </div>
           </div>
-          <div className="flex justify-end ">
+          <div className="flex md:mx-auto lg:justify-end ">
             <Card>
               <CardContent className="p-0">
                 <Image
